954f63ddeab34fe604b0757dba949155e14557ed
rgholdre <rgholdre@asu.edu>
2025-10-23 22:37:54 -0700

    Added Relations tables -- needs testing

--- database.py
+++ database.py
@@ -34,12 +34,14 @@ CREATE TABLE IF NOT EXISTS course (
 # currently doesn't follow partial keys; treats sec_no as primary
 create_section_table = """
 CREATE TABLE IF NOT EXISTS section (
-    Sec_no INT PRIMARY KEY,
+    Sec_no INT,
+    CourseId INT,
     Term VARCHAR(50),
     Year INT,
     Session CHAR(1),
     Modality VARCHAR(50),
-    Capacity INT
+    Capacity INT,
+    PRIMARY KEY (Sec_no, CourseId)
 )
 """
 
@@ -62,15 +64,61 @@ CREATE TABLE IF NOT EXISTS timeSlot (
 """
 
 # Relationship tables
-create_offered_as_table = """"""
+create_offered_as_table = """
+CREATE TABLE IF NOT EXISTS offered_as(
+    Sec_no INT,
+    CourseId INT,
+    Term VARCHAR(50),
+    Year INT,
+    Session CHAR(1),
+    Modality VARCHAR(50),
+    Capacity INT,
+    PRIMARY KEY (Sec_no, CourseId),
+    FOREIGN KEY (Sec_no) REFERENCES course ON DELETE CASCADE
+)
+"""
+create_enrolled_table = """
+CREATE TABLE IF NOT EXISTS enrolled(
+    Sec_no INT,
+    CourseId INT,
+    StudentId INT,
+    PRIMARY KEY (Sec_no, CourseId, StudentId),
+    FOREIGN KEY (StudentId) REFERENCES students,
+    FOREIGN KEY (Sec_no, CourseId) REFERENCES section   
+)
+"""
+create_scheduled_at_table = """
+CREATE TABLE IF NOT EXISTS scheduled_at(
+    Sec_no INT,
+    CourseId INT,
+    SlotId INT,
+    PRIMARY KEY (Sec_no, CourseId),
+    FOREIGN KEY (SlotId) REFERENCES timeSlot,
+    FOREIGN KEY (Sec_no, CourseId) REFERENCES section
+)
+"""
+create_located_at_table = """
+CREATE TABLE IF NOT EXISTS located_at(
+    Sec_no INT,
+    CourseId INT,
+    RoomId INT,
+    PRIMARY KEY (Sec_no, CourseId),
+    FOREIGN KEY (RoomId) REFERENCES room,
+    FOREIGN KEY (Sec_no, CourseId) REFERENCES section
+)
+"""
+create_teaches_table = """
+CREATE TABLE IF NOT EXISTS teaches(
+    Sec_no INT,
+    CourseId INT,
+    InstructorId INT,
+    PRIMARY KEY (Sec_no, CourseId),
+    FOREIGN KEY (InstructorId) REFERENCES instructor,
+    FOREIGN KEY (Sec_no, CourseId) REFERENCES section    
+)
+"""
 
-create_teaches_table = """"""
-create_enrolled_table = """"""
-create_scheduled_at_table = """"""
-create_located_at_table = """"""
-
-
-
+# Drop table queries for testing
 drop_student_table = "DROP TABLE IF EXISTS students;"
 drop_instructor_table = "DROP TABLE IF EXISTS instructor;"
 drop_course_table = "DROP TABLE IF EXISTS course;"
@@ -95,7 +143,7 @@ def runQuery(query):
         #Run query
         cursor.execute(query)
         connection.commit()
-        print("Query successfully run")
+        print(f"Query {query} successfully run")
 
     except psycopg2.Error as e:
         print(f"Error with connection: {e}")
@@ -104,3 +152,32 @@ def runQuery(query):
         cursor.close()
         connection.close()
         print("Database connection closed.")
+
+
+def createTables():
+    runQuery(create_course_table)
+    runQuery(create_section_table)
+    runQuery(create_instructor_table)
+    runQuery(create_student_table)
+    runQuery(create_timeSlot_table)
+    runQuery(create_room_table)
+
+def createRelations():
+    runQuery(create_scheduled_at_table)
+    runQuery(create_located_at_table)
+    runQuery(create_section_table)
+    runQuery(create_enrolled_table)
+    runQuery(create_offered_as_table)
+
+def closeTables():
+    runQuery(drop_course_table)
+    runQuery(drop_instructor_table)
+    runQuery(drop_room_table)
+    runQuery(drop_section_table)
+    runQuery(drop_student_table)
+    runQuery(drop_timeSlot_table)
+
+# create/close tables
+createTables()
+createRelations()
+#closeTables()
